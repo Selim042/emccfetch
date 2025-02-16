5c9115e938e09b837169bd1d561262a23857ef37
Emily (Selim) <042Selim@gmail.com>
2025-02-16 11:05:30 -0500

    Create emccfetch-installer.lua

---
+++ emccfetch-installer.lua
@@ -0,0 +1,4 @@
+-- hosted at https://pastebin.com/w8dPRNHR
+
+shell.run('wget https://raw.githubusercontent.com/Selim042/emccfetch/refs/heads/main/emccfetch.lua')
+shell.run('wget https://raw.githubusercontent.com/Selim042/emccfetch/refs/heads/main/emccfetch-lib.lua')
