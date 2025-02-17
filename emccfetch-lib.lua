fb745390ad7ef38b06ed204976f266dfe946b51b
Emily (Selim) <042Selim@gmail.com>
2025-02-17 10:15:14 -0500

    Add theme support

--- emccfetch-lib.lua
+++ emccfetch-lib.lua
@@ -1,14 +1,72 @@
+local OPTION_KEYS = {
+    THEME = 'emccfetch.theme'
+}
+local themes = {
+    logo = nil,
+    pride_horz = {
+        'eeeeeeee',
+        'eeeeeeee',
+        '11111111',
+        '44444444',
+        '55555555',
+        'bbbbbbbb',
+        'aaaaaaaa',
+        'aaaaaaaa',
+    },
+    pride_flag = {
+        'c7eeeeee',
+        '3c7eeeee',
+        '63c71111',
+        '063c7444',
+        '063c7555',
+        '63c7bbbb',
+        '3c7aaaaa',
+        'c7aaaaaa',
+    },
+    trans = {
+        '33333333',
+        '33333333',
+        '33333333',
+        '66666666',
+        '66666666',
+        '66666666',
+        '00000000',
+        '00000000',
+    },
+}
+local themeOptions = ''
+for k,v in pairs(themes) do
+    themeOptions = themeOptions .. k .. ', '
+end
+settings.define(OPTION_KEYS.THEME, {
+    description = "A possible theme to pick from. Options are: "..themeOptions,
+    default = "logo",
+    type = "string",
+})
+
+local themeSet = settings.get(OPTION_KEYS.THEME)
+local invalidTheme = true
+for k,v in pairs(themes) do
+    if (k == themeSet) then invalidTheme = false end
+end
+if (invalidTheme) then
+    print("Invalid theme selected:",themeSet)
+    print("Using default theme: logo")
+    themeSet = 'logo'
+end
+
 local theme = {
     label_text = colors.red,
     info_text = colors.white
 }
+themeOptions:sub(1,#themeOptions-3)
 local x,y = term.getSize()
 
 -- CraftOS Logo
 local logo = {
     {
         chars = "        ",
-        fg = "00000000",
+        fg = "        ",
         bg = "ffffffff"
     },
     {
@@ -32,6 +90,11 @@ local logo = {
         bg = "f4ffff4f"
     },
     {
+        chars = "        ",
+        fg = "        ",
+        bg = "f4ffff4f"
+    },
+    {
         chars = "     \140  ",
         fg = "     e  ",
         bg = "f444444f"
@@ -41,22 +104,37 @@ local logo = {
         fg = "        ",
         bg = "ffffffff"
     },
-    {
-        chars = "        ",
-        fg = "        ",
-        bg = "ffffffff"
-    }
 }
 
 local logoPadding = ' '
 for i=1,#logo[1].chars do
     logoPadding = logoPadding..' '
 end
+local function getThemedBackground(line)
+    local out = ''
+    for i=1,#logo[line].bg do
+        if (logo[line].bg:sub(i,i) ~= colors.toBlit(colors.black)) then
+            out = out .. themes[themeSet][line]:sub(i,i)
+        else
+            out = out .. colors.toBlit(colors.black)
+        end
+    end
+    return out
+end
 local function writeLogoLine(line)
+    if (line == 1) then
+        write(logoPadding)
+        return
+    end
+    line = line - 1
     if (line > #logo) then
         write(logoPadding)
     else
-        term.blit(logo[line].chars,logo[line].fg,logo[line].bg)
+        if (themeSet == 'logo') then
+            term.blit(logo[line].chars,logo[line].fg,logo[line].bg)
+        else
+            term.blit(logo[line].chars,logo[line].fg,getThemedBackground(line))
+        end
         term.setBackgroundColor(colors.black)
         term.write(' ')
     end
