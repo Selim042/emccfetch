87e536a5d4616d25037e05444ef1462818099d70
Emily (Selim) <042Selim@gmail.com>
2025-02-18 17:05:12 -0500

    Don't worry about pastebin

--- emccfetch-installer.lua
+++ emccfetch-installer.lua
@@ -1,4 +1,2 @@
--- hosted at https://pastebin.com/w8dPRNHR
-
 shell.run('wget https://raw.githubusercontent.com/Selim042/emccfetch/refs/heads/main/emccfetch.lua')
 shell.run('wget https://raw.githubusercontent.com/Selim042/emccfetch/refs/heads/main/emccfetch-lib.lua')
